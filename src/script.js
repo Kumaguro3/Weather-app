5c8ce0a78564156f0151aee67c3aa0971ac4aa93
Kumaguro3 <lily.sergent@gmail.com>
2022-07-10 10:15:35 +0900

    Fixed the ApiKey mistake in getForecast

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = `6a0b4c54d0ac0f8372ec53375213a3c8`;
+  let apiKey = "6a0b4c54d0ac0f8372ec53375213a3c8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -126,7 +126,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f476a0b4c54d0ac0f8372ec53375213a3c8";
+  let apiKey = "6a0b4c54d0ac0f8372ec53375213a3c8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
